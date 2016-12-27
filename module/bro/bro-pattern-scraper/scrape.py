c2900ff599a99a840636df8dfc0cf80354fe44b8
blacktop <blacktop@users.noreply.github.com>
2016-12-27 13:11:10 -0700

    working on scrape.py

--- module/bro/bro-pattern-scraper/scrape.py
+++ module/bro/bro-pattern-scraper/scrape.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+from requests.compat import urljoin
 import json
 import os
+import re
 
 
 def field_type_lookup(ftype, field):
@@ -36,34 +38,39 @@ def doc2grok(fields):
     return '\\t'.join(converted)
 
 
+# TODO: add edge case where it is a table of <p>
+# https://www.bro.org/sphinx/scripts/base/init-bare.bro.html#type-SOCKS::Address
 def get_nested_fields(field_name, field_type, url):
     nested = []
     resp = requests.get(url=url)
     soup = BeautifulSoup(resp.content, "html.parser")
-    for dl in soup.find_all("dl", {"class": "type"}):
-        dts = dl.find_all('dt')
-        if field_type not in dts[0].text:
-            break
-        for dt in dts[1:-1]:
-            nfield_name = dt.contents[0].split(':', 1)[0]
-            nfield_type = dt.contents[1].text
-            nested.append(dict(field=field_name+'.'+nfield_name, type=nfield_type))
+    dt_text = url.split('#')[-1]
+    dl = soup.find("dt", id=dt_text).parent.find("dl", {"class": "last docutils"})
+    if dl is not None:
+        for nfield in list(zip(dl.find_all("dt"), dl.find_all("dd"))):
+            if len(nfield) == 2:
+                nfield_name = nfield[0].contents[0].split(':', 1)[0]
+                nfield_type = nfield[0].contents[1].text
+                if nfield[1].p is not None:
+                    nfield_description = nfield[1].p.text
+                else:
+                    nfield_description = ""
+                nested.append(dict(field=field_name + '.' + nfield_name,
+                                   type=nfield_type,
+                                   description=nfield_description))
+    else:
+        nested.append(dict(field=field_name, type=field_type, description=""))
     return nested
 
 
-def scrape_bro_docs():
-    """ Crawl Bro NSM docs to extract log types """
+def get_log_types():
     url = "https://www.bro.org/sphinx/script-reference/"
     resp = requests.get(url=url + "log-files.html")
     soup = BeautifulSoup(resp.content, "html.parser")
-
     bro_logs = dict(logs=[])
     for table in soup.find_all("table", {"class": "docutils"}):
         for row in table.find('tbody').find_all('tr'):
             log = {}
-            link = row.find('a', href=True)
-            if link:
-                log['url'] = url + link['href']
             cols = row.find_all('td')
             cols = [ele.text.strip() for ele in cols]
             tds = [ele for ele in cols if ele]
@@ -71,19 +78,62 @@ def scrape_bro_docs():
             log['log_type'] = os.path.splitext(log['file'])[0]
             log['description'] = tds[1]
             log['fields'] = []
+            link = row.find('a', href=True)
+            # do not add a URL for notice_alarm.log
+            if link or 'notice_alarm' not in log['log_type']:
+                log['url'] = urljoin(url, link['href'])
             bro_logs['logs'].append(log)
+    return bro_logs
+
+
+def is_url(url):
+    regex = re.compile(r'^(?:http|ftp)s?://'  # http:// or https://
+                       r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+                       r'localhost|'  # localhost...
+                       r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+                       r'(?::\d+)?'  # optional port
+                       r'(?:/?|[/?]\S+)$',
+                       re.IGNORECASE)
+    return regex.match(url)
+
+
+def build_url(current_url, next_url):
+    if is_url(next_url):
+        return next_url
+    else:
+        return urljoin(current_url, next_url)
+
+
+# TODO: handle edge case for files.log
+# https://www.bro.org/sphinx/scripts/base/frameworks/files/main.bro.html#type-Files::Info
+def scrape_bro_docs():
+    """ Crawl bro.org docs to extract log types """
+    bro_logs = get_log_types()
+
     for log_type in bro_logs['logs']:
         if log_type.get('url', None):
-            resp = requests.get(url=log_type.get('url'))
+            resp = requests.get(url=log_type.get('url'), allow_redirects=True)
             soup = BeautifulSoup(resp.content, "html.parser")
-            for dt in soup.find_all("dt"):
-                if '&log' in dt.text:
-                    field_name = dt.contents[0].split(':', 1)[0]
-                    field_type = dt.contents[1].text
-                    if '::' in dt.contents[1].text:
-                        log_type['fields'] += get_nested_fields(field_name, field_type, log_type.get('url') + dt.a['href'])
-                    else:
-                        log_type['fields'].append(dict(field=field_name, type=field_type))
+            dt_text = log_type.get('url').split('#', 1)[1]
+            try:
+                dl = soup.find("dt", id=dt_text).parent.find("dl", {"class": "docutils"})
+                for dfield in list(zip(dl.find_all("dt"), dl.find_all("dd"))):
+                    if len(dfield) == 2:
+                        field_name = dfield[0].contents[0].split(':', 1)[0]
+                        field_type = dfield[0].contents[1].text
+                        if dfield[1].p is not None:
+                            field_description = dfield[1].p.text
+                        else:
+                            field_description = ""
+                        if '::' in field_type:
+                            url = build_url(log_type.get('url'), dfield[0].a['href'])
+                            log_type['fields'] += get_nested_fields(field_name, field_type, url)
+                        else:
+                            log_type['fields'].append(dict(field=field_name,
+                                                           type=field_type,
+                                                           description=field_description))
+            except Exception as e:
+                print e.message
     with open('bro-logs.json', 'w') as jsonfile:
         json.dump(bro_logs, jsonfile)
     return bro_logs
