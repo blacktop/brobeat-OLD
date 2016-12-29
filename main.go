0b4a338bee7c69b36f6fff31604b0cd7fdee600c
blacktop <blacktop@users.noreply.github.com>
2016-12-29 10:33:52 -0700

    now we are getting close!?

--- main.go
+++ main.go
@@ -3,27 +3,47 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 // "github.com/elastic/beats/libbeat/beat"
 // "github.com/blacktop/brobeat/beater"
 
-// BroLogFields log file header
-type BroLogFields struct {
-	Separator    string
-	SetSeparator string
-	EmptyField   string
-	UnsetField   string
-	Path         string
-	Open         string
-	Fields       []string
-	Types        []string
+// BroHeaderFields log file header
+type BroHeaderFields struct {
+	Separator    string   `json:"separator,omitempty"`
+	SetSeparator string   `json:"set_separator,omitempty"`
+	EmptyField   string   `json:"empty_field,omitempty"`
+	UnsetField   string   `json:"unset_field,omitempty"`
+	Path         string   `json:"path,omitempty"`
+	Open         string   `json:"open,omitempty"`
+	Fields       []string `json:"fields"`
+	Types        []string `json:"types"`
 }
 
-func readHeader(filePath string) BroLogFields {
+// BroField log line field
+type BroField struct {
+	Name  string `json:"name"`
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+// BroLogLine log entry
+type BroLogLine struct {
+	Type         string     `json:"type,omitempty"`
+	Created      string     `json:"created,omitempty"`
+	SetSeparator string     `json:"set_separator,omitempty"`
+	EmptyField   string     `json:"empty_field,omitempty"`
+	UnsetField   string     `json:"unset_field,omitempty"`
+	Fields       []BroField `json:"fields,omitempty"`
+}
+
+func readHeader(filePath string) BroHeaderFields {
 
 	// #separator \x09
 	// #set_separator	,
@@ -34,7 +54,7 @@ func readHeader(filePath string) BroLogFields {
 	// #fields	ts	uid	id.orig_h	id.orig_p	id.resp_h	id.resp_p	trans_depth	method	host	uri	referrer	version	user_agent	request_body_len	response_body_len	status_code	status_msg	info_code	info_msg	tags	username	password	proxied	orig_fuids	orig_filenames	orig_mime_types	resp_fuids	resp_filenames	resp_mime_types	uri_vars	cookie_vars
 	// #types	time	string	addr	port	addr	port	count	string	string	string	string	string	string	count	count	count	string	count	string	set[enum]	string	string	set[string]	vector[string]	vector[string]	vector[string]	vector[string]	vector[string]	vector[string]	vector[string]	vector[string]
 
-	blFields := BroLogFields{}
+	hFields := BroHeaderFields{}
 
 	f, err := os.Open(filePath)
 	if err != nil {
@@ -48,37 +68,39 @@ func readHeader(filePath string) BroLogFields {
 	if err = scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading header:", err)
 	}
-	blFields.Separator = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#separator"))
+	hFields.Separator = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#separator"))
 
 	_ = scanner.Scan()
 	if err = scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading header:", err)
 	}
-	blFields.SetSeparator = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#set_separator"))
+	hFields.SetSeparator = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#set_separator"))
 
 	_ = scanner.Scan()
 	if err = scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading header:", err)
 	}
-	blFields.EmptyField = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#empty_field"))
+	hFields.EmptyField = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#empty_field"))
 
 	_ = scanner.Scan()
 	if err = scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading header:", err)
 	}
-	blFields.UnsetField = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#unset_field"))
+	hFields.UnsetField = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#unset_field"))
 
 	_ = scanner.Scan()
 	if err = scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading header:", err)
 	}
-	blFields.Path = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#path"))
+	hFields.Path = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#path"))
 
 	_ = scanner.Scan()
 	if err = scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading header:", err)
 	}
-	blFields.Open = strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#open"))
+	layout := "2006-01-02-15-04-05"
+	t, _ := time.Parse(layout, strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#open")))
+	hFields.Open = t.Format(time.RFC3339Nano)
 
 	_ = scanner.Scan()
 	if err = scanner.Err(); err != nil {
@@ -87,7 +109,7 @@ func readHeader(filePath string) BroLogFields {
 	fields := strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#fields"))
 	rFields := csv.NewReader(strings.NewReader(fields))
 	rFields.Comma = '\t'
-	blFields.Fields, err = rFields.Read()
+	hFields.Fields, err = rFields.Read()
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -99,20 +121,12 @@ func readHeader(filePath string) BroLogFields {
 	types := strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#types"))
 	rTypes := csv.NewReader(strings.NewReader(types))
 	rTypes.Comma = '\t'
-	blFields.Types, err = rTypes.Read()
+	hFields.Types, err = rTypes.Read()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// if len(fieldNames) == len(fieldTypes) {
-	// 	for i, name := range fieldNames {
-	// 		lFields[name] = fieldTypes[i]
-	// 	}
-	// } else {
-	// 	fmt.Println("ERROR!!!")
-	// }
-
-	return blFields
+	return hFields
 }
 
 func main() {
@@ -120,33 +134,43 @@ func main() {
 	// if err != nil {
 	// 	os.Exit(1)
 	// }
-	fmt.Printf("%#v", readHeader("./data/http.log"))
-	os.Exit(0)
+	broHeader := readHeader("./data/http.log")
 
-	// csvFile, err := os.Open("./data/http.log")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// defer csvFile.Close()
-	//
-	// r := csv.NewReader(csvFile)
-	// r.Comma = '\t'
-	// r.Comment = '#'
-	// r.LazyQuotes = true
-	//
-	// lines, err := r.ReadAll()
-	// if err != nil {
-	// 	log.Fatalf("error reading all lines: %v", err)
-	// }
-	// for i, line := range lines {
-	// 	if i > 9 {
-	// 		break
-	// 	}
-	// 	fmt.Println(line)
-	// }
-	// recs, err := r.Read()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(recs)
+	csvFile, err := os.Open("./data/http.log")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csvFile.Close()
+
+	r := csv.NewReader(csvFile)
+	r.Comma = '\t'
+	r.Comment = '#'
+	r.LazyQuotes = true
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		log.Fatalf("error reading all lines: %v", err)
+	}
+	for _, line := range lines {
+		broLine := BroLogLine{
+			Type:         broHeader.Path,
+			Created:      broHeader.Open,
+			SetSeparator: broHeader.SetSeparator,
+			UnsetField:   broHeader.UnsetField,
+			EmptyField:   broHeader.EmptyField,
+		}
+		for j, word := range line {
+			broField := BroField{
+				Name:  broHeader.Fields[j],
+				Type:  broHeader.Types[j],
+				Value: word,
+			}
+			broLine.Fields = append(broLine.Fields, broField)
+		}
+		broJSON, err := json.Marshal(broLine)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(broJSON))
+	}
 }
