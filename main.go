d1a1bcf7de5d7501092308351bc230fd6392e3b2
blacktop <blacktop@users.noreply.github.com>
2016-12-29 01:20:47 -0700

    fix readHeader

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 // "github.com/elastic/beats/libbeat/beat"
 // "github.com/blacktop/brobeat/beater"
 
-func readHeader(filePath string) {
+func readHeader(filePath string) map[string]string {
+
 	// #separator \x09
 	// #set_separator	,
 	// #empty_field	(empty)
@@ -21,6 +22,8 @@ func readHeader(filePath string) {
 	// #open	2016-12-28-16-25-43
 	// #fields	ts	uid	id.orig_h	id.orig_p	id.resp_h	id.resp_p	trans_depth	method	host	uri	referrer	version	user_agent	request_body_len	response_body_len	status_code	status_msg	info_code	info_msg	tags	username	password	proxied	orig_fuids	orig_filenames	orig_mime_types	resp_fuids	resp_filenames	resp_mime_types	uri_vars	cookie_vars
 	// #types	time	string	addr	port	addr	port	count	string	string	string	string	string	string	count	count	count	string	count	string	set[enum]	string	string	set[string]	vector[string]	vector[string]	vector[string]	vector[string]	vector[string]	vector[string]	vector[string]	vector[string]
+
+	lFields := make(map[string]string)
 
 	f, err := os.Open(filePath)
 	if err != nil {
@@ -58,21 +61,29 @@ func readHeader(filePath string) {
 	fields := strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#fields"))
 	rFields := csv.NewReader(strings.NewReader(fields))
 	rFields.Comma = '\t'
-	record, err := rFields.Read()
+	fieldNames, err := rFields.Read()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(record)
 
 	_ = scanner.Scan()
 	types := strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "#types"))
 	rTypes := csv.NewReader(strings.NewReader(types))
 	rTypes.Comma = '\t'
-	record, err = rTypes.Read()
+	fieldTypes, err := rTypes.Read()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(record)
+
+	if len(fieldNames) == len(fieldTypes) {
+		for i, name := range fieldNames {
+			lFields[name] = fieldTypes[i]
+		}
+	} else {
+		fmt.Println("ERROR!!!")
+	}
+
+	return lFields
 }
 
 func main() {
@@ -80,7 +91,7 @@ func main() {
 	// if err != nil {
 	// 	os.Exit(1)
 	// }
-	readHeader("./data/http.log")
+	fmt.Println(readHeader("./data/http.log"))
 	os.Exit(0)
 
 	csvFile, err := os.Open("./data/http.log")
@@ -90,24 +101,19 @@ func main() {
 	defer csvFile.Close()
 
 	r := csv.NewReader(csvFile)
-
-	// r := NewFieldsReader(csvFile, 0, 2)
 	r.Comma = '\t'
 	r.Comment = '#'
 	r.LazyQuotes = true
+
 	lines, err := r.ReadAll()
 	if err != nil {
 		log.Fatalf("error reading all lines: %v", err)
 	}
 	for i, line := range lines {
-		if i == 0 {
-			// skip header line
-			continue
-		}
-		fmt.Println(line)
 		if i > 9 {
 			break
 		}
+		fmt.Println(line)
 	}
 	// recs, err := r.Read()
 	// if err != nil {
